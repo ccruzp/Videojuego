725dd6b149b59a7af31305b722b3479a9ec46f2b
ccruzp <carlos@Carlos-PC.(none)>
2014-07-20 01:38:56 -0430

    Refactorizado el código de time.js

--- time.js
+++ time.js
@@ -1,70 +1,45 @@
+var width = 800; // Width of the game
+var height = 600; // Height of the game
 
-var width = 800;
-var height = 600;
+var cursors;
+var fireButton;
+var shieldUp;
+var shieldDown;
 
+var player; // The player
+var alien; // Instance of an enemy
+var aliens; // Group that represents the enemies
+var bullet; // Instance of a bullet shot by an enemy
+var bullets; // Group that represents the bullets shot by enemies
+var shieldTime = 0; // Time that takes the shield to activate
+var bulletSpeed = 1; // Speed of the bullets
+var bulletWaitingTime = 0; // Avoids too many bullets shot at the same time
+var lectureSpeedTime = 0; // Avoids keyboard lecture too many times
 
-var game = new Phaser.Game(width, height, Phaser.AUTO, 'phaser-test', { preload: preload, create: create, update: update });
+var score = 0; // NOT USED YET - Player's score
+var scoreText; // NOT USED YET - Display for player's score
+var speedText; // Display for enemies' bullet's speed
+var shieldText; // Display for player's shield's time
 
+var shielded = false; // Says if the player is shielded or not
 
+// Variable that represents the game
+var game = new Phaser.Game(
+    width, height, Phaser.AUTO, 'phaser-test', 
+    { preload: preload, create: create, update: update }
+);
 
 function preload() {
-    game.load.image('sky', 'assets/img/background2.png');
-    game.load.image('ground', 'assets/platform.png');
+    game.load.image('background', 'assets/img/background2.png');
     game.load.image('star', 'assets/shielded.png');
-    game.load.image('diamond', 'assets/diamond.png');
-    game.load.image('firstaid', 'assets/firstaid.png');
-    game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
-    game.load.spritesheet('enemy', 'assets/stuff.png', 105, 84);
-    game.load.spritesheet('base', 'assets/basesheet2.png', 95, 100,2);
     game.load.image('bullet', 'assets/bullet.png', 32, 48);
+    game.load.spritesheet('base', 'assets/basesheet2.png', 95, 100,2);
 }
 
-
-var ship;
-var star;
-var firstaid;
-var diamond;  
-var diamondTime = 0;
-var speedTime = 0;
-var projectileSpeed = 1;
-var platforms;
-var ground;
-var score = 0;
-var scoreText;
-var speedText;
-var shieldText;
-var shieldInterval = 0;
-var fireButton;
-var enemy;
-var player2;
-var alien;
-var shieldSpeed = 0;
-var shieldup;
-var shielddown;
-var shielded = false;
-
 function create() {
+    game.add.sprite(0, 0, 'background');
 
-    // The platforms group contains the ground and the 2 ledges we can jump on
-    platforms = game.add.group();
-
-    // We will enable physics for any object that is created in this group
-    platforms.enableBody = true;
-    
-    // Here we create the ground
-    ground = platforms.create(0, game.world.height - 64, 'ground');
-
-    // // Scale it to fit the width of the game (the original sprite is 400x32)
-    ground.scale.setTo(2, 2);
-
-    // // This stops it from falling away when you jump on it
-    ground.body.immovable = true;
-    
-    
-    game.add.sprite(0, 0, 'sky');
-
-
-    makeGrid(width,height);
+    make_Grid(width,height);
 
     // The player and its settings
     // player = game.add.sprite(32, game.world.height - 150, 'dude');
@@ -74,8 +49,6 @@ function create() {
     game.physics.arcade.enable(player);
 
     // Player physics properties. Give the little guy a slight bounce
-    player.body.bounce.y = 0.2;
-    player.body.gravity.y = 300;
     player.body.collideWorldBounds = true;
 
     //  The baddies!
@@ -83,45 +56,72 @@ function create() {
     aliens.enableBody = true;
     aliens.physicsBodyType = Phaser.Physics.ARCADE;
 
-    createAliens();
+    alien = aliens.create(300, 60, 'star');
+    alien.anchor.setTo(0.5, 0.5);
 
     speedText = game.add.text(
         16, height-32, '', { font: '24px Arial', fill: '#ffffff' }
     );
 
-
     shieldText = game.add.text(
         16, height-32-36, '', { font: '24px Arial', fill: '#ffffff' }
     );
 
-
-
-
-    diamonds = game.add.group();
-    diamonds.enableBody = true;
-    diamonds.physicsBodyType = Phaser.Physics.ARCADE;
-    diamonds.createMultiple(30, 'bullet');
-    diamonds.setAll('anchor.x', 0.5);
-    diamonds.setAll('anchor.y', 1);
-    diamonds.setAll('outOfBoundsKill', true);
-    diamonds.setAll('checkWorldBounds', true);
+    bullets = game.add.group();
+    bullets.enableBody = true;
+    bullets.physicsBodyType = Phaser.Physics.ARCADE;
+    bullets.createMultiple(30, 'bullet');
+    bullets.setAll('anchor.x', 0.5);
+    bullets.setAll('anchor.y', 1);
+    bullets.setAll('outOfBoundsKill', true);
+    bullets.setAll('checkWorldBounds', true);
 
     cursors = game.input.keyboard.createCursorKeys();
     fireButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
-    shieldup = game.input.keyboard.addKey(Phaser.Keyboard.W);
-    shielddown = game.input.keyboard.addKey(Phaser.Keyboard.S);
-
-    
-
-
+    shieldUp = game.input.keyboard.addKey(Phaser.Keyboard.W);
+    shieldDown = game.input.keyboard.addKey(Phaser.Keyboard.S);
 }
 
-function makeGrid(width, height) {
+function update() {
+    game.physics.arcade.overlap(player, bullets, check_Shield, null, this);
+
+    // Reset the players velocity (movement)
+    player.body.velocity.x = 0;
+
+    if (cursors.left.isDown) {
+	player.body.velocity.x = -150;
+    } else if (cursors.right.isDown) {
+	player.body.velocity.x = 150;
+    } 
+
+    if (shielded) {
+        player.frame = 1;
+    } else {
+        player.frame = 0;
+    }
+    if (cursors.up.isDown || cursors.down.isDown) {
+        update_Shield_Speed();
+    }
+    if (shieldUp.isDown || shieldDown.isDown) {
+        update_Bullet_Speed();
+    }
+    if (fireButton.isDown) {
+        shoot();
+        if (shieldTime > 0){
+            game.time.events.add(Phaser.Timer.SECOND * shieldTime * 0.90, activate_Shield, this);
+            game.time.events.add(Phaser.Timer.SECOND * shieldTime + Phaser.Timer.SECOND * 0.1, deactivate_Shield, this);
+        }
+    }
+    speedText.text = 'Velocidad: ' + bulletSpeed;
+    shieldText.text = 'Escudo: ' + shieldTime + ' s.' ;
+}
+
+function make_Grid(width, height) {
     var graphics = game.add.graphics(0, 0);
     
     //space between lines
-    space = 20;
+    var space = 20;
     
     //adding lines
     graphics.lineStyle(1, 0x33FF00,0.5);
@@ -137,159 +137,58 @@ function makeGrid(width, height) {
         end--;
         game.add.text(width-space,y-10,String(end),style);
     }
-    
-    
-
-
 }
 
-function update() {
-
-    game.physics.arcade.collide(firstaid, platforms);
-    game.physics.arcade.overlap(aliens, diamonds, killBoth, null, this);
-    game.physics.arcade.overlap(player, diamonds, checkShield, null, this);
-
-    // Reset the players velocity (movement)
-    player.body.velocity.x = 0;
-
-    if (cursors.left.isDown) {
-
-    player.body.velocity.x = -150;
-
-    } else if (cursors.right.isDown) {
-
-    player.body.velocity.x = 150;
-
-    } 
-
-
-    if (shielded) {
-        player.frame = 1;
-    } else {
-
-        player.frame = 0;
-    }
-
-
-
-
-
-
-   
-    if (cursors.up.isDown || cursors.down.isDown) {
-        updateSpeed();
-    }
-
-
-    if (shieldup.isDown || shielddown.isDown) {
-        updateSpeed2();
-    }
-
-    if (fireButton.isDown) {
-        fireDiamond2();
-
-        if (shieldSpeed > 0){
-            game.time.events.add(Phaser.Timer.SECOND * shieldSpeed*0.90, activateShield, this);
-            game.time.events.add(Phaser.Timer.SECOND * shieldSpeed + Phaser.Timer.SECOND * 0.1, deactivateShield, this);
+function update_Shield_Speed() {
+    if (game.time.now > lectureSpeedTime) {
+        if (cursors.down.isDown && (bulletSpeed > 1)){
+            bulletSpeed = bulletSpeed -1;
+            lectureSpeedTime = game.time.now + 150;
         }
-        
-
-    }
-    speedText.text = 'Speed: ' + projectileSpeed;
-    shieldText.text = 'Shield: ' + shieldSpeed + ' segs.' ;
-}
-
-
-function updateSpeed(){
-    if (game.time.now > speedTime) {
-        if (cursors.down.isDown && (projectileSpeed > 1)){
-            projectileSpeed= projectileSpeed -1;
-            speedTime = game.time.now + 150;
-        }
-        if (cursors.up.isDown && (projectileSpeed < 30)){
-            projectileSpeed = projectileSpeed + 1;
-            speedTime = game.time.now + 150;
+        if (cursors.up.isDown && (bulletSpeed < 30)){
+            bulletSpeed = bulletSpeed + 1;
+            lectureSpeedTime = game.time.now + 150;
         }
     }
 }
 
-function updateSpeed2(){
-    if (game.time.now > speedTime) {
-        if (shielddown.isDown && (shieldSpeed > 1)){
-            shieldSpeed= shieldSpeed -1;
-            speedTime = game.time.now + 150;
+function update_Bullet_Speed() {
+    if (game.time.now > lectureSpeedTime) {
+        if (shieldDown.isDown && (shieldTime > 1)){
+            shieldTime= shieldTime -1;
+            lectureSpeedTime = game.time.now + 150;
         }
-        if (shieldup.isDown && (shieldSpeed < 30)){
-            shieldSpeed = shieldSpeed + 1;
-            speedTime = game.time.now + 150;
+        if (shieldUp.isDown && (shieldTime < 30)){
+            shieldTime = shieldTime + 1;
+            lectureSpeedTime = game.time.now + 150;
         }
     }
 }
 
+function shoot() {
+    if (game.time.now > bulletWaitingTime) {
+        bullet = bullets.getFirstExists(false);
+        if (bullet) {
+            bullet.reset(alien.x, alien.y + alien.height/2 + 15);
+            bullet.body.velocity.y = 20 * bulletSpeed;
+            bulletWaitingTime = game.time.now + 200;
+        }
+    }
+}
 
-function activateShield(){
+function activate_Shield() {
     shielded = true;
 }
 
-function deactivateShield(){
+function deactivate_Shield() {
     shielded = false;
 }
 
-
-
-function fireDiamond() {
-
-    if (game.time.now > diamondTime) {
-        diamond = diamonds.getFirstExists(false);
-        if (diamond) {
-            diamond.reset(player.x + player.width/2, player.y);
-            diamond.body.velocity.y = -20 * projectileSpeed;
-            diamondTime = game.time.now + 200;
-        }
-    }
-}
-
-
-function fireDiamond2() {
-
-    if (game.time.now > diamondTime) {
-        diamond = diamonds.getFirstExists(false);
-        if (diamond) {
-            diamond.reset(alien.x, alien.y + alien.height/2 + 15);
-            diamond.body.velocity.y = 20 * projectileSpeed;
-            diamondTime = game.time.now + 200;
-        }
-    }
-}
-
-
-
-function createAliens () {
-
-
-    alien = aliens.create(200, 50, 'star');
-    alien.anchor.setTo(0.5, 0.5);
-    alien.body.moves = false;
-
-    aliens.x = 0;
-    aliens.y = 0;
-
-
-    alien.x = 300;
-    alien.y = 60;
-
-}
-
-function killBoth(alien, diamond) {
-    //alien.kill();
-    // diamond.kill();
-}
-
-function checkShield(player,diamond){
+function check_Shield(player, bullet) {
     if (!shielded){
         player.kill();
     } else {
-        diamond.kill();
+        bullet.kill();
     }
 
 }
