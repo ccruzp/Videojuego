8359f36e6f2f0705a4828ed86d297a60cbef83ed
Gamar <gamarazuaje@gmail.com>
2014-08-21 12:37:38 -0430

    Initial commit

---
+++ Boot.js
@@ -0,0 +1,65 @@
+var BasicGame = {};
+
+BasicGame.Boot = function (game) {
+
+};
+
+BasicGame.Boot.prototype = {
+
+    preload: function () {
+
+        //  Here we load the assets required for our preloader (in this case a background and a loading bar)
+	this.load.image('background', 'assets/img/background_purple_Half.png');
+        // this.load.image('background', 'assets/Mockup/PNGs/background.png');
+        // this.load.image('preloaderBar', 'images/preloadr_bar.png');
+	// The bomb that follows the mouse (Perhaps this will be removed)
+	this.load.image('bombSelect', 'assets/bullet.png');
+	// The bomb
+	this.load.image('bomb', 'assets/bullet.png');
+	// The distance enemy
+	this.load.image('enemyDistance', 'assets/Mockup/PNGs/distanceShip.png');
+	this.load.image('playAgainButton', 'assets/star.png');
+	this.load.image('startButton', 'assets/star.png');
+	this.load.image('playButton', 'assets/Mockup/PNGs/playButton.png');
+	this.load.image('lockedButton', 'assets/Mockup/PNGs/lock.png');
+	this.load.image('minusButton', 'assets/Mockup/PNGs/minusButton.png');
+	this.load.image('plusButton', 'assets/Mockup/PNGs/plusButton.png');
+	this.load.spritesheet('blackHoleButton', 'assets/Spritesheets/Button_bomb_sheet.png', 250, 242, 2);
+	this.load.image('missileButton', 'assets/Mockup/PNGs/missileButton.png');
+	this.load.image('ground', 'assets/platform2.png');
+    },
+
+    create: function () {
+
+    //     //  Unless you specifically know your game needs to support multi-touch I would recommend setting this to 1
+    //     this.input.maxPointers = 1;
+
+    //     // //  Phaser will automatically pause if the browser tab the game is in loses focus. You can disable that here:
+    //     // this.stage.disableVisibilityChange = true;
+
+    //     // if (this.game.device.desktop)
+    //     // {
+    //     //     //  If you have any desktop specific settings, they can go in here
+    //     //     this.scale.pageAlignHorizontally = true;
+    //     // }
+    //     // else
+    //     // {
+    //     //     //  Same goes for mobile settings.
+    //     //     //  In this case we're saying "scale the game, no lower than 480x260 and no higher than 1024x768"
+    //     //     this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    //     //     this.scale.minWidth = 480;
+    //     //     this.scale.minHeight = 260;
+    //     //     this.scale.maxWidth = 1024;
+    //     //     this.scale.maxHeight = 768;
+    //     //     this.scale.forceLandscape = true;
+    //     //     this.scale.pageAlignHorizontally = true;
+    //     //     this.scale.setScreenSize(true);
+    //     // }
+
+    //     //  By this point the preloader assets have loaded to the cache, we've set the game settings
+    //     //  So now let's start the real preloader going
+        this.state.start('Preloader');
+
+    }
+
+};
---
+++ GameOverMenu.js
@@ -0,0 +1,50 @@
+BasicGame.GameOverMenu = function (game) {
+    this.gameOverText = null;
+    this.playAgainButton = null;
+};
+
+BasicGame.GameOverMenu.prototype = {
+
+    preload: function () {
+	
+    },
+    
+    create: function () {
+	
+	this.gameOverText = this.add.text(this.world.centerX, 50, '¡Perdiste!',
+					  { font: "50px Arial", fill: "#ffffff",
+					    align: "left" });
+	this.gameOverText.anchor.setTo(0.5, 0.5);
+
+	this.playAgainButton = this.add.button(100, 100, 'startButton',
+					  this.fun, this);
+	this.playAgainButton.anchor.setTo(0.5, 0.5);
+	
+    },
+    
+    update: function () {
+    
+    // 		//	You don't actually need to do this, but I find it gives a much smoother game experience.
+    // 		//	Basically it will wait for our audio file to be decoded before proceeding to the MainMenu.
+    // 		//	You can jump right into the menu if you want and still play the music, but you'll have a few
+    // 		//	seconds of delay while the mp3 decodes - so if you need your music to be in-sync with your menu
+    // 		//	it's best to wait for it to decode here first, then carry on.
+    
+    // 		//	If you don't have any music in your game then put the game.state.start line into the create function and delete
+    // 		//	the update function completely.
+    
+    // 	// 	if (this.cache.isSoundDecoded('titleMusic') && this.ready == false)
+    // 	// 	{
+    // 			// this.ready = true;
+    // 			// this.state.start('MainMenu');
+    // 	// 	}
+    
+    },
+
+    fun: function () {
+	this.gameOverText.destroy();
+	this.playAgainButton.destroy();
+	this.state.start('Distance');
+    }
+    
+};
