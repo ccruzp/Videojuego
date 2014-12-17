377a31609e6e1b6556526b6fa7b9a99d64c07211
Carlos <carlos@carlos-PC.(none)>
2014-12-17 08:18:41 -0430

    Moviendo el botón de play

--- MainMenu.js
+++ MainMenu.js
@@ -311,7 +311,7 @@ BasicGame.MainMenu.prototype = {
 
     // Creates the play button
     playButton_Setup: function() {
-	this.playButton = this.add.button(this.world.centerX, this.world.height - 60, 'playButton', this.start, this, 0, 0, 1, 0);
+	this.playButton = this.add.button(this.world.centerX + 10, this.world.height - 60, 'playButton', this.start, this, 0, 0, 1, 0);
 	this.playButton.anchor.setTo(0.5, 0.5);
 	this.playButton.scale.setTo(0.1, 0.1);
 	buttons.add(this.playButton);
